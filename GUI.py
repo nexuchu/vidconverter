33c5cd883a030c76a578525e050f7ca7a603bfd0
Nexu <nexuget@gmail.com>
2024-04-29 18:34:00 +0300

    Added youtube to MP4 downloader

--- GUI.py
+++ GUI.py
@@ -3,15 +3,18 @@ from tkinter import filedialog
 
 
 class App(customtkinter.CTk):
-    def __init__(self, on_convert_callback):
+    def __init__(self, on_convert_callback, download_youtube):
         super().__init__()
         self.on_convert_callback = on_convert_callback
+        self.download_youtube = download_youtube
 
         self.title("Media Converter")
         self.geometry("700x450")
 
-        # set default format
+        # set default values
         self.format = '.mp4'
+        self.res = '144p'
+        self.showprogress = False
 
         # set grid layout 1x2
         self.grid_rowconfigure(0, weight=1)
@@ -32,7 +35,7 @@ class App(customtkinter.CTk):
                                                    anchor="w", command=self.home_button_event)
         self.home_button.grid(row=1, column=0, sticky="ew")
 
-        self.frame_2_button = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="Audio from video",
+        self.frame_2_button = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="Youtube to MP4/MP3",
                                                       fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),
                                                     anchor="w", command=self.frame_2_button_event)
         self.frame_2_button.grid(row=2, column=0, sticky="ew")
@@ -53,14 +56,14 @@ class App(customtkinter.CTk):
         self.home_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
         self.home_frame.grid_columnconfigure(0, weight=1)
 
-        self.home_frame_large_image_label = customtkinter.CTkLabel(self.home_frame, text="")
-        self.home_frame_large_image_label.grid(row=0, column=0, padx=20, pady=20)
+        self.home_frame_large_label = customtkinter.CTkLabel(self.home_frame, text="Convert any video to different video/audio formats!")
+        self.home_frame_large_label.grid(row=0, column=0, padx=20, pady=20)
 
         self.home_frame_button_1 = customtkinter.CTkButton(self.home_frame, text="Choose videos", command=self.select_file)
         self.home_frame_button_1.grid(row=1, column=0, padx=20, pady=10)
         self.home_frame_button_2 = customtkinter.CTkButton(self.home_frame, text="Output folder", command=self.select_output)
         self.home_frame_button_2.grid(row=2, column=0, padx=20, pady=10)
-        self.home_frame_optionframe = customtkinter.CTkOptionMenu(self.home_frame, values=['.mp4', '.mpg', '.mov'], command=self.optionmenu_callback, width=50)
+        self.home_frame_optionframe = customtkinter.CTkOptionMenu(self.home_frame, values=['.mp4', '.mpg', '.mov', '.mp3'], command=self.optionmenu_callback, width=50)
         self.home_frame_optionframe.grid(row=3, column=0, padx=20, pady=10)
         self.home_frame_entry = customtkinter.CTkEntry(self.home_frame, placeholder_text="Title (no spaces)")
         self.home_frame_entry.grid(row=4, column=0, padx=20, pady=10)
@@ -69,6 +72,20 @@ class App(customtkinter.CTk):
 
         # create second frame
         self.second_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
+        self.second_frame.grid_columnconfigure(0, weight=1)
+
+        self.second_frame_label = customtkinter.CTkLabel(self.second_frame, text="Paste your youtube link below:")
+        self.second_frame_label.grid(row=0, column=0, padx=20, pady=20)
+        self.second_frame_entry = customtkinter.CTkEntry(self.second_frame, placeholder_text="URL")
+        self.second_frame_entry.grid(row=1, column=0, padx=20, pady=10)
+        self.second_frame_optionframe = customtkinter.CTkOptionMenu(self.second_frame, values=['144p', '360p', '720p', '1080p'], command=self.optionmenu_yt_callback, width=50)
+        self.second_frame_optionframe.grid(row=2, column=0, padx=20, pady=10)  
+        self.second_frame_button_1 = customtkinter.CTkButton(self.second_frame, text="Output folder", command=self.select_output)
+        self.second_frame_button_1.grid(row=3, column=0, padx=20, pady=10)      
+        self.second_frame_button_2 = customtkinter.CTkButton(self.second_frame, text="Download", command=self.Download)
+        self.second_frame_button_2.grid(row=4, column=0, padx=20, pady=10)
+        self.second_frame_progress_label = customtkinter.CTkLabel(self.second_frame, text="0%")
+        self.second_frame_progress_bar = customtkinter.CTkProgressBar(self.second_frame)
 
         # create third frame
         self.third_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
@@ -112,6 +129,17 @@ class App(customtkinter.CTk):
 
     def optionmenu_callback(self, choice):
         self.format = choice
+    
+    def optionmenu_yt_callback(self, choice):
+        self.res = choice
+    
+    def progress_visibility(self):
+        if self.showprogress == True:
+            self.second_frame_progress_label.grid(row=5, column=0, padx=20, pady=0)
+            self.second_frame_progress_bar.grid(row=6, column=0, padx=20, pady=10)
+        else:
+            self.second_frame_progress_label.grid_forget()
+            self.second_frame_progress_bar.grid_forget()
 
     def select_file(self): #for the file chooser popup, chooses the file and changes the button
         self.files = [x.replace("/", "\\") for x in list(filedialog.askopenfilenames())]
@@ -119,25 +147,40 @@ class App(customtkinter.CTk):
 
     
     def select_output(self): #for the file chooser popup to choose the output path, chooses the path(folder)and changes the button
-        self.pathname = filedialog.askdirectory().replace('/', f'\\')
-        self.home_frame_button_2.configure(text=f"Output folder chosen ({self.pathname})", fg_color="green")
+        self.outputpath = filedialog.askdirectory().replace('/', f'\\')
+        self.home_frame_button_2.configure(text=f"Output folder chosen ({self.outputpath})", fg_color="green")
+        self.second_frame_button_1.configure(text=f"Output folder chosen ({self.outputpath})", fg_color="green")
 
-    
+    def Percentage(self, percentage):
+        self.second_frame_progress_label.configure(text=f"{int(percentage)}%")
+        self.second_frame_progress_label.update()
+        self.second_frame_progress_bar.set(float(percentage/100))
+
     def Convert(self): #checks if the data provided is enough, if yes, gives data to main py file
         self.name = self.home_frame_entry.get()
         self.home_frame_entry.delete(first_index=0, last_index=len(self.name))
-        if self.files and self.name and self.pathname != "":
+        if self.files and self.name and self.outputpath != "":
             print(self.name)
             print(self.files)
-            print(self.pathname)
+            print(self.outputpath)
             self.home_frame_button_1.configure(text="Choose videos", fg_color='#1F6AA5')
             self.home_frame_button_2.configure(text="Output folder", fg_color="#1F6AA5")
             self.home_frame_button_4.configure(text="Converting...")
-            self.on_convert_callback(self.name, self.files, self.pathname, self.format)
+            self.on_convert_callback(self.name, self.files, self.outputpath, self.format)
         else:
             self.home_frame_button_4.configure(text="Missing required parameters! Retry!")
         
-
+    def Download(self):
+        self.link = self.second_frame_entry.get()
+        self.second_frame_entry.delete(first_index=0, last_index=len(self.link))
+        if self.link and self.outputpath != '':
+            print(self.link)
+            print(self.res)
+            print(self.outputpath)
+            self.download_youtube(self.link, self.res, self.outputpath)
+            self.second_frame_button_1.configure(text="Output folder", fg_color="#1F6AA5")
+        else:
+            self.home_frame_button_2.configure(text="Missing required parameters! Retry!")
 
 if __name__ == "__main__":
     app = App()
--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-import ffmpeg
-import customtkinter
-import tkinter as tk
 import os
 from GUI import App
-from ffmpeg import FFmpeg, Progress
+from ffmpeg import FFmpeg
+from pytube import YouTube
+from threading import Thread
 
-def on_convert(name, files, path, format): #converts the provided video to mp4 format
+def on_convert(name, files, path, format): # converts the video(s) to the provided format
     print(rf"{path}\\{name}.mp4")
     filecount = len(files)
     for num, file_path in enumerate(files):
@@ -15,8 +14,7 @@ def on_convert(name, files, path, format): #converts the provided video to mp4 f
             .option("y")
             .input(file_path)
             .output(
-                f"{path}\\{name}-{num+1}{format}",
-                codec="copy"
+                f"{path}\\{name}-{num+1}{format}"
             )
         )
 
@@ -28,7 +26,39 @@ def on_convert(name, files, path, format): #converts the provided video to mp4 f
 
         ffmpeg.execute()
 
+def download_worker(yt, path): # downloads the video and updates GUI
+    app.showprogress = True
+    app.progress_visibility()
+    app.second_frame_button_2.configure(text="Downloading...")
+    yt.download(path)
+    app.showprogress = False
+    app.progress_visibility()
+    app.second_frame_button_2.configure(text="Downloaded video!")
+    os.system(f'start {os.path.realpath(path)}')
+
+def on_progress(stream, chunk, bytes_remaining): # updates progress bar info
+    total_size = stream.filesize
+    bytes_downloaded = total_size - bytes_remaining
+    percentage_completed = bytes_downloaded / total_size * 100
+    app.Percentage(percentage_completed)
+
+def download_youtube(link, res, path): # checks if the info provided is correct, if so, downloads the video threaded
+    try:
+        yt = YouTube(link, on_progress_callback=on_progress)
+        if res != '1080p':
+            yt = yt.streams.get_by_resolution(res)
+        else:
+            yt = yt.streams.get_highest_resolution()
+        if yt:
+            th = Thread(target=download_worker, daemon=True, args=(yt, path))
+            th.start()
+        else:
+            raise ValueError("No video streams found for set resolution")
+    except Exception as e: 
+        app.second_frame_button_2.configure(text=f"An error has occurred: {e}")
+        print(e)
+
 
 if __name__ == "__main__":
-    app = App(on_convert)
+    app = App(on_convert, download_youtube)
     app.mainloop()
